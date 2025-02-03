52ee28a8c7d7e2491533328dc67029ff64299e48
amanjaiswal7236 <amanjaiswal7236@gmail.com>
2025-02-04 01:51:24 +0530

    build fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "no-var": "off", // Disables the no-var rule globally
+    },
+  },
 ];
 
 export default eslintConfig;
